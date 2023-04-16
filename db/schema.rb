efbdc3a920204d5a0aaf404516d362d97e0452e6
sousuke <suguruyoshida.0807@gmail.com>
2023-04-16 22:12:41 +0900

    [add]contractorモデルの追加
    ユーザーが元請を作成し、保存しておくためのモデルを追加。ユーザーとの関連付けも追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_16_070732) do
+ActiveRecord::Schema.define(version: 2023_04_16_083855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contractors", force: :cascade do |t|
+    t.string "name", limit: 30, null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_contractors_on_user_id"
+  end
 
   create_table "one_time_authentications", force: :cascade do |t|
     t.integer "function_name", null: false
@@ -59,5 +67,6 @@ ActiveRecord::Schema.define(version: 2023_04_16_070732) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contractors", "users"
   add_foreign_key "sites", "users"
 end

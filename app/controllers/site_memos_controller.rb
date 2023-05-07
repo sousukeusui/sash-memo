fa5bdf7ea090088ee3f68e421dc05ceeed0be9a5
sousuke <suguruyoshida.0807@gmail.com>
2023-05-07 22:42:10 +0900

    [update]メモ追加機能の変更
    セッションを使っていたところをクラスインスタンスにするなど色々変更

--- app/controllers/site_memos_controller.rb
+++ app/controllers/site_memos_controller.rb
@@ -1,5 +1,4 @@
 class SiteMemosController < ApplicationController
-  before_action :new_step1_validation, only: :divide_form_by_kind
 
   def index(site_id:)
     @site = Site.find(site_id)
@@ -7,10 +6,20 @@ class SiteMemosController < ApplicationController
   end
 
   def new_step1(site_id:)
-    session[:site_id] = site_id
+    @@site_id = site_id
   end
 
   def divide_form_by(kind_number:)
+    construction_kind = ConstructionKind.find_by(kind_number: kind_number)
+    
+    @@site_memo = SiteMemo.new(
+      construction_kind_id: construction_kind.id,
+      room: '1Fリビング',
+      site_id: @@site_id
+    )
+    
+    redirect_to site_memos_new_step1_path(@@site_id), notice: @@site_memo.errors.full_messages unless @@site_memo.valid?
+
     case kind_number
       when 'inner_sash'
         redirect_to site_memos_new_step2_inner_sash_path
@@ -21,21 +30,10 @@ class SiteMemosController < ApplicationController
     
   end
 
-  def new_step3_inner_sash
-    
+  def new_step3_inner_sash(room:, width_up_size:, width_middle_size:, width_down_size:,
+                            height_left_size:, height_middle_size:, height_right_size:,
+                            height_frame_depth:, width_frame_depth:)
+    @@site_memo.room = room
   end
 
-
-  private 
-
-  def new_step1_validation(kind_number:)
-    construction_kind = ConstructionKind.find_by(kind_number: kind_number)
-    site_memo = SiteMemo.new(
-      construction_kind_id: construction_kind.id,
-      room: "洋室１",
-      site_id: session[:site_id]
-    )
-
-    redirect_to site_memos_new_step1_path(session[:site_id]), notice: site_memo.errors.full_messages unless site_memo.valid?
-  end
 end
--- app/models/site_memo.rb
+++ app/models/site_memo.rb
@@ -1,4 +1,9 @@
 class SiteMemo < ApplicationRecord
   belongs_to :construction_kind
   belongs_to :site
+
+  validates :room, presence: true, length: { maximum: 15 }
+  validates :site_id, presence: true
+  validates :construction_kind_id, presence: true
+
 end

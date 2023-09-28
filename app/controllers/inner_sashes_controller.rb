a5c00edd17c20737fa094bdb34fb4957ce1272dd
sousuke <suguruyoshida.0807@gmail.com>
2023-09-28 22:23:00 +0900

    [update]現場メモが二重に作られないように修正
    site_memoモデルのデータがinner_sashモデルのデータを作るごとに作られてしまっていたため修正

--- app/controllers/inner_sashes_controller.rb
+++ app/controllers/inner_sashes_controller.rb
@@ -4,18 +4,18 @@ class InnerSashesController < ApplicationController
     @site_id = site_id
   end
 
-  def new_step3
-   
+  def new_step3(site_id:)
+
   end
 
   def room_append(room:, width_up_size:, width_middle_size:, width_down_size:,
                   height_left_size:, height_right_size:, height_middle_size:,
                   height_frame_depth:, width_frame_depth:, site_id:)
-    #インスタンスを作成
     site_memo = SiteMemo.new(kind: :inner_sash, site_id: site_id)
+    site_memo = SiteMemo.find_by(site_id: site_id) if SiteMemo.exists?(site_id: site_id)
     inner_sash = site_memo.inner_sashes.build(room: room, width_up_size: width_up_size, width_middle_size: width_middle_size, width_down_size: width_down_size,
-                                height_left_size: height_left_size, height_middle_size: height_middle_size, height_right_size: height_right_size,
-                                height_frame_depth: height_frame_depth, width_frame_depth: width_frame_depth)
+                                              height_left_size: height_left_size, height_middle_size: height_middle_size, height_right_size: height_right_size,
+                                              height_frame_depth: height_frame_depth, width_frame_depth: width_frame_depth)
     return redirect_to inner_sashs_new_step2_path, notice: inner_sash.errors.full_messages unless inner_sash.save!
     @inner_sash = inner_sash.attributes
   end

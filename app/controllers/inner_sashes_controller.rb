bfb602e169047868faa2fd58c69309eefe0fd8b9
sousuke <suguruyoshida.0807@gmail.com>
2023-11-29 09:25:40 +0900

    [add]内窓ガラスデータ作成機能追加
    内窓のガラス情報を保存するための機能を追加

--- app/controllers/inner_sashes_controller.rb
+++ app/controllers/inner_sashes_controller.rb
@@ -16,6 +16,10 @@ class InnerSashesController < ApplicationController
     @site_memo = SiteMemo.find(site_memo_id)
   end
 
+  def new_step6(site_memo_id:)
+    
+  end
+
   def room_append(room:, width_up_size:, width_middle_size:, width_down_size:,
                   height_left_size:, height_right_size:, height_middle_size:,
                   height_frame_depth:, width_frame_depth:, site_id:)
@@ -43,8 +47,8 @@ class InnerSashesController < ApplicationController
 
   def glass_append
     site_memo  = SiteMemo.find(params[:site_memo][:id])
-    return redirect_to inner_sashes_new_step6_path(params[:site_memo][:id]) if site_memo.update()
-    return redirect_to inner_sashes_new_step5_path(params[:site_memo][:id]), notice: site_memo.erros.full_messages
+    return redirect_to inner_sashes_new_step6_path(params[:site_memo][:id]) if site_memo.update!(glass_info_params)
+    return redirect_to inner_sashes_new_step5_path(params[:site_memo][:id]), notice: site_memo.errors.full_messages
   end
   
   private
@@ -56,5 +60,10 @@ class InnerSashesController < ApplicationController
   def accesory_info_params
     params.require(:site_memo).permit(:id,
                                       inner_sashes_attributes: [:key_height, :middle_frame_height, :is_flat_bar, :is_adjust, :id])
+  end
+
+  def glass_info_params
+    params.require(:site_memo).permit(:id,
+                                      inner_sashes_attributes: [:glass_thickness, :glass_kind, :glass_color, :is_low_e, :id])
   end
 end

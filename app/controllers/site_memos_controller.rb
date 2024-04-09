f198f2647c2cfea15b76edc64e2fc2f7791b4b09
sousuke <suguruyoshida.0807@gmail.com>
2024-04-09 12:06:01 +0900

    [update]メモ削除のフラッシュメッセージ表示。その他リファクタリング
    メモを削除した際にフラッシュメッセージが表示されるようにした。その他気になったところのリファクタリング

--- app/controllers/site_memos_controller.rb
+++ app/controllers/site_memos_controller.rb
@@ -10,9 +10,8 @@ class SiteMemosController < ApplicationController
 
   def update_bulk_order(site_id:,order:)
     @site = Site.find(site_id)
-    @site_memos = @site.site_memos
-    @site_memos.update_all(order: order)
-    @site_memos = @site_memos.page(params[:page]).per(5)
+    @site.site_memos.update_all(order: order)
+    @site_memos = @site.site_memos.page(params[:page]).per(5)
     @order_key = get_order_key(site_memos: @site.site_memos)
     flash.now.notice = "全て#{ SiteMemo.orders_i18n[order.to_sym]}にしました"
   end
@@ -36,7 +35,7 @@ class SiteMemosController < ApplicationController
   def destroy(id:)
     @site_memo = SiteMemo.find(id)
     @site_memo.destroy
-    # flash.now.notice = 'メモを削除しました'
+    flash.now.notice = 'メモを削除しました'
   end
 
   private

23d26942f032cc2af7979979ab8d82dfc5ceff69
sousuke <suguruyoshida.0807@gmail.com>
2024-05-01 22:12:59 +0900

    [update]不要なコメントの削除

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -5,11 +5,9 @@ class Contractor < ApplicationRecord
   has_many :sites
 
   validates :name, presence: true, length: {maximum: 30}
-
-  # contractor.methodでひらがなかカタカナかを判定し、変換したい
   
   scope :suggest_name, -> (name:){ suggest_hira_name(name: name).or(suggest_kana_name(name: name)) }
-  
+
   scope :suggest_hira_name, -> (name:){ where('name LIKE ? ', "%#{sanitize_sql_like(name.to_s.to_hira)}%") }
   scope :suggest_kana_name, -> (name:){ where('name LIKE ? ', "%#{sanitize_sql_like(name.to_s.to_kana)}%") }
 end

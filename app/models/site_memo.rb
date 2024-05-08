2090733429bb133be53a1946618929691346e9e4
sousuke <suguruyoshida.0807@gmail.com>
2024-05-08 12:27:52 +0900

    [update]不要なところの削除
    不要なところを削除した

--- app/models/site_memo.rb
+++ app/models/site_memo.rb
@@ -5,10 +5,9 @@ class SiteMemo < ApplicationRecord
 
   belongs_to :site
   has_one :inner_sash, dependent: :destroy
-  accepts_nested_attributes_for :inner_sash
 
   validates :kind, presence: true
-  validates :status ,presence: true
+  validates :status, presence: true
   validates :room, presence: true, length: { maximum:15 }
   validates :remark, length: { maximum:100 }
 

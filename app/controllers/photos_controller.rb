225937f9e74e63964b01e590c201a003f0d26f05
sousuke <suguruyoshida.0807@gmail.com>
2024-05-19 00:28:30 +0900

    [update]cocoon導入で不要になった箇所の削除
    cocoonを導入し、turboで行っていたフォームの追加・削除が不要になったので関連ファイルを削除

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,2 @@
 class PhotosController < ApplicationController
-  def append_photo_form(id:)
-    @inner_sash = InnerSash.find(id)
-    @form_index = Time.now.to_i
-  end
-
-  # すでにある写真データのフォーム削除
-  def destroy_existing_photo_form(id:)
-    @photo = Photo.find(id)
-    @photo.destroy
-  end
-
-  #新しく作った写真フォームの削除
-  def destroy_new_photo_form(index:)
-    @index = index
-  end
 end

1c5d2730bce5633e7497a2de2704dbe420b4323e
sousuke <suguruyoshida.0807@gmail.com>
2024-05-23 16:08:47 +0900

    [fix]発注状況表示のバグ修正
    発注状況が正常に表示できないバグがあったので修正

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,17 +1,14 @@
 module SitesHelper
-  def check_schedule(date)
+  def check_schedule(date:)
     return '未定' if date == nil
     return l date, format: :short
   end
 
-  def check_order(site_memos)
+  def check_order(site_memos:)
     return if site_memos.blank?
-    order = []
-    site_memos.each do |site_memo|
-      # site_memoの子テーブルが出来次第ここに処理追加
-      order << site_memo.inner_sashes.pluck(:order) if site_memo.kind == 'inner_sash'
-    end
-    return '発注済み' if order.all?{|n| n == 'ordered'}
+    # site_memoの子テーブルが出来次第ここに処理追加
+    orders = site_memos.flat_map{ |site_memo| site_memo.inner_sashes&.pluck(:order)}
+    return '発注済み' if orders.all?{|n| n == 'ordered'}
     return '未発注'
   end
 end

6ae7b0a4885661269f28b2a0518f3f15a6d27511
sousuke <suguruyoshida.0807@gmail.com>
2024-06-06 14:46:21 +0900

    [add]スマホ用UI追加
    スマホに適したUIを追加

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -8,7 +8,7 @@ module SitesHelper
     return if site_memos.blank?
     # site_memoの子テーブルが出来次第ここに処理追加
     orders = site_memos.flat_map{ |site_memo| site_memo.inner_sashes&.pluck(:order)}
-    return '発注済み' if orders.all?{|n| n == 'ordered'}
+    return '発注済' if orders.all?{|n| n == 'ordered'}
     return '未発注'
   end
 end

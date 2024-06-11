f92082d94d8624115d650538539aa28a17a6f68f
sousuke <suguruyoshida.0807@gmail.com>
2024-06-11 15:22:21 +0900

    [update]発注・未発注リンクのバグ修正
    前後のページに移動するナビゲーションを使用した際に発注・未発注を切り替えるリンクが
    うまく動いていなかったので修正

--- app/controllers/inner_sashes_controller.rb
+++ app/controllers/inner_sashes_controller.rb
@@ -66,9 +66,10 @@ class InnerSashesController < ApplicationController
   end
 
   def destroy(id:)
+    p id
     inner_sash = InnerSash.find(id)
     inner_sash.destroy_last_with(site_memo: inner_sash.site_memo)
-    redirect_to site_memos_index_path(site_id: site_memo.site_id), notice: "メモを削除しました"
+    redirect_to site_memos_index_path(site_id: inner_sash.site_memo.site_id), notice: "メモを削除しました"
   end
 
   def navigate_page(id:)
